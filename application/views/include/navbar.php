9fe051de660d13de49c62dd7e4b42f48bef946e8
hafiz <rajitasemmy@gmail.com>
2022-07-10 17:03:42 +0700

    change navbar.php display Dibuat oleh Hafiz

--- application/views/include/navbar.php
+++ application/views/include/navbar.php
@@ -112,7 +112,7 @@
     		<!-- Footer -->
     		<div class="legal">
     			<div class="copyright">
-    				&copy; 2020 <a href="javascript:void(0);">Created By Anisa</a>.
+    				&copy; 2020 <a href="javascript:void(0);">Dibuat Oleh Hafiz</a>.
     			</div>
     			<div class="version">
     				<b>Version: </b> 1.0
